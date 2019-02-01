52d28547cb6eeb8c60f65786e57195b1c5a9dd99
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-02-01 16:03:59 +0100

    Random number set

--- app/src/main/java/com/aloine/viewmodelconfig/MainActivity.kt
+++ app/src/main/java/com/aloine/viewmodelconfig/MainActivity.kt
@@ -2,11 +2,24 @@ package com.aloine.viewmodelconfig
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var data: MainActivityDataGenerator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        data = MainActivityDataGenerator()
+        val myRandomNumber = data.getNumber()
+        textview_random_number.text = myRandomNumber
+        Log.i(TAG, "Random Number set")
+
+
+    }
+    companion object {
+        private val TAG: String? = MainActivity::class.simpleName
+
     }
 }
